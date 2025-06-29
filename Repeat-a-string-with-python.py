f15571bb8fbf98e399a34b25d9910e28fde0f375
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-06-29 08:39:27 -0700

    Repeat-a-string-with-python
    This is a simple project that repeat string.
    
    At first, get input for number of string and repetitions and then output the result.

---
+++ Repeat-a-string-with-python.py
@@ -0,0 +1,7 @@
+
+# Get input for number of string and repetitions
+s =input("Please enter the string: ")
+n = int(input("Please enter the number of repetitions: "))
+
+# output the result
+print(s*n)
